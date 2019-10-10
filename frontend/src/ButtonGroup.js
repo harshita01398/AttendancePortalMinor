ac4300cd2cc2a45e2371df0b19be6777fe7f9b97
adityakaushik27 <adityakaushik27@gmail.com>
2019-10-10 18:37:07 +0530

    Convert button added

--- frontend/src/ButtonGroup.js
+++ frontend/src/ButtonGroup.js
@@ -1,18 +1,24 @@
 import React from 'react';
 import Loading from './Loading';
 import styles from './Layout.css';
+
 class ButtonGroup extends React.Component{
     constructor(props){
         super(props);
         this.state = { file: '', loading: false ,file_name:''};
-        this.uploadFile = this.uploadFile.bind(this);
         this.reader = new FileReader();
+        this.uploadFile = this.uploadFile.bind(this);
+        this.convertFile = this.convertFile.bind(this);
     }
 
     uploadFile(e){
-        this.setState({file_name:e.target.files[0].name})
+        this.setState({file_name:e.target.files[0].name});
         this.reader.readAsDataURL(e.target.files[0]);
-        console.log(e.target.files[0].name);
+        // console.log(e.target.files[0].name);
+    }
+
+    convertFile(e){
+        console.log(e.target);
     }
 
     componentDidMount(){ // Life cycle method.
@@ -33,17 +39,20 @@ class ButtonGroup extends React.Component{
     render(){
         return(
             <div>
+                <div>
+                    <img src="background.jpg" style={{ position: 'absolute', height: 'auto', width: 'auto' }} />
+                    <div style={{ position: 'absolute' , top: '50px' , left:'40%' , color: 'azure' , fontSize: '80px' }}>eXtractor</div>
+                </div>
             {
                 this.state.loading ? <Loading progress={this.state.loading}/> :
                 <div  className="choose">
-                    <label className="label" for ="img">Select an attendance sheet.</label>
-                    <br></br><br></br>
-                    <input type='file' accept={'image/* video/*'}  onChange={this.uploadFile} id = "img" style={{display:"none"}} />
+                    <label className="label" htmlFor={"img"}>Select an attendance sheet.</label>
+                    <input type='file' accept={'image/*'}  onChange={this.uploadFile} id = "img" style={{display:"none"}} />
                     <h4 style={{color : "lightblue"}}>{this.state.file_name}</h4>
                     <img src={this.state.file} alt = "No sheet chosen"  style = {{width : '700px',height: '400px'}} />
-                    <br></br>
-                    <br></br>
-                    <button className="convert"> Convert </button>
+                    <div>
+                        <button className="convert" disabled={!this.state.file} onClick={this.convertFile}> Convert </button>
+                    </div>
                 </div>
             }
             </div>
