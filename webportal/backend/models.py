a5e58ce02e1623f40357fa8884e22ed2373597de
Harshita <harshita01398@gmail.com>
2019-11-26 10:06:43 +0530

    js files added
    Signed-off-by: Harshita <harshita01398@gmail.com>

--- webportal/backend/models.py
+++ webportal/backend/models.py
@@ -2,15 +2,6 @@ from django.db import models
 
 # Database models
 
-# class Info(models.Model):
-#     SubCode = models.CharField(max_length=200)
-#     SubName = models.CharField(max_length=200)
-#     TCode = models.CharField(max_length=200)
-#     TeacherName = models.CharField(max_length=200)
-#     Dept = models.CharField(max_length=200)
-
-#     def __str__(self):
-#         return self.SubCode
 
 class Record(models.Model):
     Auto_increment_id = models.AutoField(primary_key=True)
